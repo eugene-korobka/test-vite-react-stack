63f3dd926f2057f351e9c6aedad96d7cc838097d
Eugene Korobka <eugene.korobka@uinno.io>
2023-01-31 11:40:03 +0200

    fixed MainContainer component

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,6 +1,6 @@
 export function MainContainer({ children }: { children?: React.ReactNode }) {
   return (
-    <main className="max-w-7xl my-0 mx-auto p-6 text-center">
+    <main className="max-w-7xl my-0 mx-auto p-6">
       {children}
     </main>
   );
