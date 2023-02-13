8125a40753d000f2e24d0d20da9f0cba25901f36
Eugene Korobka <eugene.korobka@uinno.io>
2023-02-13 16:07:34 +0200

    hid modal manager test panel

--- src/pages/ItemsList/ItemsListPage.tsx
+++ src/pages/ItemsList/ItemsListPage.tsx
@@ -1,19 +1,19 @@
 import { FilteredItemsListWidget } from 'pages/ItemsList/FilteredItemsListWidget/FilteredItemsListWidget';
 
-import { ButtonWithModalReduxV1, ButtonWithModalReduxV2, ButtonWithModalReduxV3 } from 'components/ModalManager/ModalManager';
+// import { ButtonWithModalReduxV1, ButtonWithModalReduxV2, ButtonWithModalReduxV3 } from 'components/ModalManager/ModalManager';
 
 export const ItemsListPage = () => {
   return (
     <div className="w-full">
       <h2 className="mb-4 text-xl">Items List Page</h2>
-      <div>
+      {/* <div>
         <div>Modal Manager test panel</div>
         <div className='flex gap-4'>
           <ButtonWithModalReduxV1 />
           <ButtonWithModalReduxV2 />
           <ButtonWithModalReduxV3 />
         </div>
-      </div>
+      </div> */}
       <FilteredItemsListWidget />
     </div>
   );
