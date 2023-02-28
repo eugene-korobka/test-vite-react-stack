f42446d2aa86663cb8376c3f8252a2ed48f94a71
Eugene Korobka <eugene.korobka@uinno.io>
2023-02-28 10:57:24 +0200

    refactored sidebar

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom";
+import { AppRoutes } from "src/routes";
+
+const sidebarItems = [
+  {
+    to: AppRoutes.home(),
+    title: 'Home',
+  },
+  {
+    to: AppRoutes.itemsList(),
+    title: 'Items',
+  },
+  // {
+  //   to: AppRoutes.ownersList(),
+  //   title: 'Owners',
+  // },
+];
+
+function getNavLinkClassName({ isActive }) {
+  return `block px-3 py-2 rounded-sm text-blue-400${ isActive ? ' bg-gray-200 text-gray-700' : ''}`;
+};
+
+export const AppSidebar = () => {
+  return (
+    <div className="w-50 sticky top-0 p-3">
+      {/* <h3 className="w-full mb-4 text-center">Sidebar</h3> */}
+      <nav>
+        <ul className="m-0 p-0 list-none text-gra">
+          {sidebarItems.map(({ to, title }) => (
+            <li key={to}>
+              <NavLink className={getNavLinkClassName} to={to}>{title}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,6 +1,6 @@
-import { Link, Outlet } from "react-router-dom";
-import { AppRoutes } from 'src/routes';
+import { Outlet } from "react-router-dom";
 
+import { AppSidebar } from "components/AppSidebar";
 import { MainContainer } from "components/MainContainer";
 
 export const RootPage = () => {
@@ -8,19 +8,7 @@ export const RootPage = () => {
     <MainContainer>
       <h1 className="mb-4 text-center text-2xl font-bold">React stack</h1>
       <div className="flex">
-        <div className="w-50 sticky top-0 p-6">
-          <h3 className="w-full mb-4 text-center">Sidebar</h3>
-          <nav>
-            <ul className="m-0 p-0 list-none">
-              <li>
-                <Link to={AppRoutes.home()}>Home page</Link>
-              </li>
-              <li>
-                <Link to={AppRoutes.itemsList()}>Items list page</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <AppSidebar />
         <div className="flex-1">
           <Outlet />
         </div>
