a97b659fac07e8cc8f4411a96f46685ba4e96274
Eugene Korobka <eugene.korobka@uinno.io>
2023-03-06 12:31:51 +0200

    fixed SelectOwner

--- src/widgets/SelectOwner.widget.tsx
+++ src/widgets/SelectOwner.widget.tsx
@@ -16,26 +16,32 @@ export const SelectOwner = (props: SelectOwnerProps) => {
 
   const hasItems = !isFetching && data?.length;
 
+  const isSelectVisible = !isFetching;
+
   const ownerInputId = useId();
 
   return (
     <label htmlFor={ownerInputId} className="block mb-6">
       <span className="block mb-2">Owner</span>
-      <select
-        id={ownerInputId}
-        className="block w-full p-2 border border-solid border-gray-400 rounded-md"
-        {...register(ownerIdInputName, { setValueAs: (v) => Number(v) || null })}
-      >
-        <option key={-1} value="">
-          Select owner...
-        </option>
-        {hasItems &&
-          data.map((owner) => (
-            <option key={owner.id} value={owner.id}>
-              {getOwnerFullName(owner)}
-            </option>
-          ))}
-      </select>
+      {isFetching && <div className="w-full p-2 border border-solid border-gray-400 rounded-md">Loading...</div>}
+      {isSelectVisible && (
+        <select
+          id={ownerInputId}
+          className="block w-full p-2 border border-solid border-gray-400 rounded-md"
+          {...register(ownerIdInputName, { setValueAs: (v) => Number(v) || null })}
+          disabled={isFetching}
+        >
+          <option key={-1} value="">
+            Select owner...
+          </option>
+          {hasItems &&
+            data.map((owner) => (
+              <option key={owner.id} value={owner.id}>
+                {getOwnerFullName(owner)}
+              </option>
+            ))}
+        </select>
+      )}
     </label>
   );
 };
