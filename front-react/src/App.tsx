be9a3ca5f8009a4772059ad3ad098af017174eef
Eugene Korobka <eugene.korobka@uinno.io>
2024-07-08 10:45:46 +0300

    added shared repo

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -1,13 +1,26 @@
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
+import { AppComponent, askQuestion, getAnswer, IconBiohazard, universe } from 'shared-test-eugene';
 
 import { store } from 'store/store';
 
 import { appRouter } from './router';
 
+function testShared() {
+  console.log('Question: ', askQuestion());
+
+  console.log('Answer: ', getAnswer());
+
+  console.log('Universe: ', universe);
+}
+
 function App() {
+  testShared();
+
   return (
     <Provider store={store}>
+      <AppComponent />
+      <IconBiohazard />
       <RouterProvider router={appRouter} />
     </Provider>
   );
